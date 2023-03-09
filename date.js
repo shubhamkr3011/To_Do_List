bdfdf53a198f531126db461a83d9d321b7c5e01b
Shubham <shubhamkumar301100@gmail.com>
2023-03-09 16:51:53 +0530

    First Commit

---
+++ date.js
@@ -0,0 +1,19 @@
+
+exports.getDate = ()=>{
+    const today = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
+    return day;
+}
+exports.getDay = ()=>{
+    const today = new Date();
+    const options = {
+        weekday: "long",
+    };
+    let day = today.toLocaleDateString("en-US", options);
+    return day;
+}
